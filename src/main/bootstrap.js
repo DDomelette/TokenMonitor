c15fe241ce3470f717c83a4ff0d300139e7132fa
DDomelette <86914232+DDomelette@users.noreply.github.com>
2026-08-05 05:18:50 +0800

    fix: acquire instance lock before store recovery

--- src/main/bootstrap.js
+++ src/main/bootstrap.js
@@ -2,17 +2,24 @@ const { app, dialog, shell } = require('electron');
 const storeModule = require('./store');
 const { runStoreBootstrap } = require('./core/startup-recovery');
 
-app.whenReady()
-  .then(() => runStoreBootstrap({
-    app,
-    dialog,
-    shell,
-    storeModule,
-    loadMain: () => require('./index'),
-    logger: console
-  }))
-  .catch(() => {
-    console.error('[bootstrap]', JSON.stringify({ code: 'BOOTSTRAP_FAILED' }));
-    app.isQuitting = true;
-    app.quit();
-  });
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.isQuitting = true;
+  app.quit();
+} else {
+  app.whenReady()
+    .then(() => runStoreBootstrap({
+      app,
+      dialog,
+      shell,
+      storeModule,
+      loadMain: () => require('./index'),
+      logger: console
+    }))
+    .catch(() => {
+      console.error('[bootstrap]', JSON.stringify({ code: 'BOOTSTRAP_FAILED' }));
+      app.isQuitting = true;
+      app.quit();
+    });
+}
